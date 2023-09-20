84fab1d521a94738321d64cff23f380d44a98f2b
Kelvin Oliveira <63024154+KelvinSilvaDev@users.noreply.github.com>
2023-09-20 17:15:18 -0300

    Cleaning comments

--- src/app/components/FormConfig.jsx
+++ src/app/components/FormConfig.jsx
@@ -3,7 +3,7 @@ import { useClientContext } from "../contexts/ClientContext";
 import { getSetting, createSetting, updateSetting } from "../services/api";
 import Modal from "react-modal";
 import { DatePickerComponent } from "./DatePicker";
-import { Form, Input, Select, Button, Space, Col, Row } from "antd";
+import { Form, Input, Select, Button, Col, Row } from "antd";
 import { CustomerSelector } from "./CustomerSelector";
 import moment from "moment";
 const { Option } = Select;
@@ -115,9 +115,6 @@ export const FormConfig = () => {
       setModalIsOpen(true);
     } else {
       console.log(form.getFieldsValue());
-      // createSetting(formData).then((response) => {
-      //   // Lógica após a criação (por exemplo, redirecionar)
-      // });
     }
   };
 
@@ -151,25 +148,20 @@ export const FormConfig = () => {
     };
 
     if (editMode) {
-      // console.log("UPDATE SETTING")
       updateSetting(JSON.stringify(requestData)).then((response) => {
         console.log(response);
         setModalIsOpen(false);
         if (response.status === 200) {
           setEditMode(true);
           console.log(response.data.description);
-          // closeModal();
         }
       });
     } else {
-      // console.log("CREATE SETTING")
       createSetting(JSON.stringify(requestData)).then((response) => {
         console.log(response);
         setModalIsOpen(false);
         if (response.status === 200) {
           setEditMode(true);
-          // closeModal();
-          // alert(response.data.description)
         }
       });
     }
@@ -232,10 +224,6 @@ export const FormConfig = () => {
       lastCleaning: lastCleaning,
       bdLastCleaning: bdLastCleaning,
     });
-
-   
-
-    // openModal();
 
   };
 
